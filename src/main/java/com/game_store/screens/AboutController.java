7dfa692aaabb6813f5dbd436329dec10b6ef5c69
k-em-o <ahmdmahamd010@gmail.com>
2025-12-14 13:07:02 +0200

    a

--- src/main/java/com/game_store/screens/AboutController.java
+++ src/main/java/com/game_store/screens/AboutController.java
@@ -1,5 +1,19 @@
 package com.game_store.screens;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
 public class AboutController {
-  
+      
+        @FXML
+    private StackPane contentArea;
+    
+    @FXML
+    public void initialize() {
+        NavbarController navbar = NavbarController.getInstance();
+        if (navbar != null) {
+            navbar.setContentArea(contentArea);
+        }
+    }
+
 }
